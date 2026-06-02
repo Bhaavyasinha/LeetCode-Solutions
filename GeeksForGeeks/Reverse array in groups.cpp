6966945adc661ed1ca68a668f87f379d1f47082d
BHAAVYA SINHA <bhaavyasinha00@gmail.com>
2026-06-02 18:25:23 +0530

    Add reverseInGroups function for array reversal
    Implement reverseInGroups function to reverse array in groups of size k.

---
+++ GeeksForGeeks/Reverse array in groups.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void reverseInGroups(vector<int> &arr, int k) {
+        // code here
+        int temp;
+        if (k >= arr.size()){
+            int temp;
+            int start = 0;
+            int end = arr.size
+            while(start > end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            }
+        }
+        else {
+            for (int i = 0; i < arr.size(); i = i + k){
+                int temp;
+                int start;
+                
+            }
+        }
+
+    }
+};
