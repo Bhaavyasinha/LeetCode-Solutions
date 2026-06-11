9fa28e4cc96a37658d03b1e2115e3a6c0d14a959
BHAAVYA SINHA <bhaavyasinha00@gmail.com>
2026-06-11 23:08:00 +0530

    Update Stock Buy and Sell – Max one Transaction Allowed.cpp

--- GeeksForGeeks/Stock Buy and Sell – Max one Transaction Allowed.cpp
+++ GeeksForGeeks/Stock Buy and Sell – Max one Transaction Allowed.cpp
@@ -18,3 +18,29 @@ class Solution {
         return maxprofit;
     }
 };
+/*------------------------------------------------------------------------------
+the code the works
+--------------------------------------------------------------------------------*/
+
+  
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        // code here
+        int minprice = prices[0];
+        int maxprofit = 0;
+        int n = prices.size();
+        for (int i = 0; i < n; i++){
+            if (prices[i] < minprice){
+                minprice = prices[i];
+            }
+            else{
+                if (prices[i] - minprice > maxprofit){
+                    maxprofit = prices[i] - minprice;
+                }
+                
+            }
+        }
+        return maxprofit;
+    }
+};
